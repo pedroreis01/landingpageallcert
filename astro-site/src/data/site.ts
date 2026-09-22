6966febefc3ee1fdacbe8c7cbe41bf433a62422a
Pedro Reis <preissouza@gmail.com>
2026-09-22 19:53:27 -0300

    fix: atualizar mapa oficial AllCert LTDA e endereço físico no Centro de BH

--- astro-site/src/data/site.ts
+++ astro-site/src/data/site.ts
@@ -13,7 +13,7 @@ export const site = {
   city: 'Belo Horizonte - MG',
   gtmId: 'GTM-P4JFXQJT',
   mapsEmbed:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.4480209638274!2d-43.93795472599831!3d-19.947659438122463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa699d5d14f8d19%3A0x31d58b155b1b366f!2sAllcert%20certificadora!5e0!3m2!1spt-BR!2sbr!4v1748611053417!5m2!1spt-BR!2sbr',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3751.0344607951815!2d-43.9409521!3d-19.9229526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa6990fe04d5f1b%3A0xea16a531624d4c0f!2sAllCert%20Certificados%20Digitais%20LTDA!5e0!3m2!1spt-BR!2sbr!4v1790117514112!5m2!1spt-BR!2sbr',
 } as const;
 
 export const nav = [
